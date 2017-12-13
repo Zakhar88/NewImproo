f09a8dcb21103bbfd6f968538b13990ecb670d66
Zakhar Garan <zharan@softserveinc.com>
2017-12-13 19:04:48 +0200

    Fixed main layout.

--- Improo/Controller/ItemViewController.swift
+++ Improo/Controller/ItemViewController.swift
@@ -18,7 +18,6 @@ class ItemViewController: AdvertisementViewController {
     @IBOutlet weak var openURLButton: UIButton?
     @IBOutlet weak var googleButton: UIButton?
     @IBOutlet weak var imageBorderView: UIView?
-    @IBOutlet weak var topSeparatorView: UIView?
     @IBOutlet weak var separatorView: UIView?
     
     var selectedItem: Item!
@@ -40,9 +39,6 @@ class ItemViewController: AdvertisementViewController {
         imageBorderView?.layer.borderColor = UIColor.mainThemeColor.cgColor
         imageBorderView?.layer.borderWidth = 2
         separatorView?.backgroundColor = UIColor.mainThemeColor
-        topSeparatorView?.backgroundColor = UIColor.mainThemeColor
-        openURLButton?.layer.cornerRadius = 5
-        googleButton?.layer.cornerRadius = 5
         view.backgroundColor = UIColor.mainThemeColor
     }
     
